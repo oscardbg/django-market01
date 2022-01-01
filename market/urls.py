3b30c187db28b73717802fc1f46a7f2001327e4d
Bruce Wayne <brucewayne@mail.com>
2022-01-01 14:39:26 -0500

    Initial item list, register and login page

--- market/urls.py
+++ market/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import login
+from .views import home, item_list, register_user, login_user
 from django.urls import path
-from .views import home
 
 app_name = 'market'
 
 urlpatterns = [
 	path('', home, name='home'),
+	path('item/', item_list, name='items'),
+	path('register/', register_user, name='register'),
+	path('login/', login_user, name='login')
 ]
--- market/views.py
+++ market/views.py
@@ -1,4 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from market.models import Item
 
 def home(request):
 	return render(request, 'market/index.html')
+
+def item_list(request):
+	item_list = Item.objects.all()
+	context = {
+		'item_list': item_list
+	}
+	return render(request, 'market/item_list.html', context)
+
+def register_user(request):
+	return render(request, 'market/register.html')
+
+def login_user(request):
+	return render(request, 'market/login.html')
